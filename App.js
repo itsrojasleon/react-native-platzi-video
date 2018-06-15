08872586cc6653a932d88f0787df27a373812c82
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-06-15 18:52:05 -0500

    Removing component and styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
@@ -13,11 +13,3 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
