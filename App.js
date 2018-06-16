72316a8b23b8d89742170d35b128462ff202dd3f
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-06-16 00:03:18 -0500

    fetching the api data

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import API from './utils/api';
+
 // Containers (Smart Components)
 import Home from './containers/home';
 import SuggestionList from './containers/suggestion-list';
@@ -9,6 +11,9 @@ import SuggestionList from './containers/suggestion-list';
 import Header from './components/header';
 
 export default class App extends React.Component {
+  async componentDidMount() {
+    const movies = await API.getSuggestion(10);
+  }
   render() {
     return (
       <Home>
