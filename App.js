786df2c815f6f8c4a9abbbe8b577ba18dfdc333a
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-06-15 19:19:21 -0500

    header component added

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { Text, View } from 'react-native';
+
+// Containers (Smart Components)
+import Home from './containers/home';
+
+// Components (Dumb components)
+import Header from './components/header';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Header />
+        <Home>
+          <Text>Something...</Text>
+        </Home>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Platform.OS === 'ios' ? 'purple' : 'gray',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ containers/home.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return this.props.children;
+  }
+}
+export default Home;
