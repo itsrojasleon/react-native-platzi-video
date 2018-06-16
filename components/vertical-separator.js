80f0c3687d277bee390962aa1b64c6c9c9259d43
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-06-15 22:59:49 -0500

    Separators and empty lists

---
+++ components/vertical-separator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const VerticalSeparator = ({ color }) => {
+  return (
+    <View style={[
+      styles.separator,
+      {borderTopColor: color ? color : '#eaeaea'},
+    ]}
+    >
+      <Text>Separator</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  separator: {
+    borderTopWidth: 1,
+  }
+});
+
+export default VerticalSeparator;
--- containers/suggestion-list.js
+++ containers/suggestion-list.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { FlatList, Text } from 'react-native';
 
 import Layout from '../components/suggestion-list-layout';
+import Empty from '../components/empty';
+import Separator from '../components/vertical-separator';
 
 class SuggestionList extends Component {
+  renderEmpty() {
+    return <Empty
+      text="No suggestions 😱"
+    />
+  }
+  renderSeparator() {
+    return <Separator />
+  }
   render() {
     const list = [
       {title: 'react', key: '1'},
-      {title: 'vue', key: '2'},
+      {title: 'c', key: '2'},
+      {title: 'd', key: '3'}
     ];
     return (
       <Layout
@@ -15,6 +26,8 @@ class SuggestionList extends Component {
       >
         <FlatList
           data={list}
+          ListEmptyComponent={this.renderEmpty}
+          ItemSeparatorComponent={this.renderSeparator}
           renderItem={({ item }) => <Text>{item.title}</Text>}
         />
       </Layout>
